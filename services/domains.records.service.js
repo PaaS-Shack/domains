4eedf26def7fdc46118274772836fed720297dbb
FLYBYME <mangoraft@gmail.com>
2022-12-27 07:11:05 -0500

    first commit

---
+++ services/domains.records.service.js
@@ -0,0 +1,403 @@
+"use strict";
+
+const DbService = require("db-mixin");
+const ConfigLoader = require("config-mixin");
+
+const { MoleculerClientError } = require("moleculer").Errors;
+
+/**
+ * attachments of addons service
+ */
+module.exports = {
+	name: "domains.records",
+	version: 1,
+
+	mixins: [
+		DbService({}),
+		ConfigLoader(['domains.**'])
+	],
+
+	/**
+	 * Service dependencies
+	 */
+	dependencies: [
+		{ name: "domains", version: 1 }
+	],
+
+	/**
+	 * Service settings
+	 */
+	settings: {
+		rest: "/v1/domains/:domain/records",
+
+		fields: {
+			id: {
+				type: "string",
+				primaryKey: true,
+				secure: true,
+				columnName: "_id"
+			},
+
+
+			domain: {
+				type: "string",
+				empty: false,
+				onCreate: ({ ctx, params }) => ctx.call("v1.domains.getDomain", { fqdn: params.fqdn, member: ctx.meta.userID })
+					.then((domain) => {
+						if (!domain) {
+							throw new MoleculerClientError(
+								`You have no right for the domain '${params.fqdn}'`,
+								403,
+								"ERR_NO_PERMISSION",
+								{ domain: params.fqdn }
+							)
+						}
+						return domain.id
+					}),
+				readonly: true,
+				populate: {
+					action: "v1.domains.get",
+					params: {
+						fields: ["id", "domain"]
+					}
+				},
+				validate: "validateDomain",
+			},
+
+			fqdn: {
+				type: "string",
+				required: true,
+				immutable: true,
+				lowercase: true,
+				trim: true,
+				empty: false,
+			},
+
+			type: {
+				type: "enum",
+				values: ["A", "AAAA", "CNAME", "SOA", "MX", "NS", "TXT", "CAA", "SRV"],
+				immutable: true,
+				required: true,
+			},
+			data: {
+				type: "string",
+				required: true,
+			},
+			network: {
+				type: "string",
+				default: null,
+				trim: true,
+				immutable: true,
+				required: false,
+			},
+			replace: {
+				type: "string",
+				required: false,
+			},
+
+			ttl: {
+				type: "number",
+				default: 99,
+				required: false,
+			},
+			priority: {
+				type: "number",
+				default: 5,
+				required: false,
+			},
+
+			//SRV
+			weight: {
+				type: "number",
+				required: false,
+			},
+			port: {
+				type: "number",
+				required: false,
+			},
+			target: {
+				type: "string",
+				required: false,
+			},
+
+			flag: {
+				type: "number",
+				default: 0,
+				required: false,
+			},
+			tag: {
+				type: "string",
+				required: false,
+			},
+
+			admin: {
+				type: "string",
+				required: false,
+			},
+			serial: {
+				type: "number",
+				required: false,
+			},
+			refresh: {
+				type: "number",
+				required: false,
+			},
+			retry: {
+				type: "number",
+				required: false,
+			},
+			expiration: {
+				type: "number",
+				required: false,
+			},
+			minimum: {
+				type: "number",
+				required: false,
+			},
+
+			nullified: {
+				type: "boolean",
+				default: false,
+				required: false,
+			},
+			poison: {
+				type: "boolean",
+				default: false,
+				required: false,
+			},
+
+
+			options: { type: "object" },
+			createdAt: {
+				type: "number",
+				readonly: true,
+				onCreate: () => Date.now()
+			},
+			updatedAt: {
+				type: "number",
+				readonly: true,
+				onUpdate: () => Date.now()
+			},
+			deletedAt: {
+				type: "number",
+				readonly: true,
+				hidden: "byDefault",
+				onRemove: () => Date.now()
+			}
+		},
+
+		scopes: {
+			async domain(query, ctx, params) { return this.validateHasDomainPermissions(query, ctx, params) },
+
+			notDeleted: { deletedAt: null }
+		},
+
+		defaultScopes: ["domain", "notDeleted"]
+	},
+
+	/**
+	 * Actions
+	 */
+
+	actions: {
+		create: {
+			permissions: ['domains.records.create']
+		},
+		list: {
+			permissions: ['domains.records.list'],
+			params: {
+				domain: { type: "string" }
+			}
+		},
+
+		find: {
+			rest: "GET /find",
+			permissions: ['domains.records.find'],
+			params: {
+				domain: { type: "string" }
+			}
+		},
+
+		count: {
+			rest: "GET /count",
+			permissions: ['domains.records.count'],
+			params: {
+				domain: { type: "string" }
+			}
+		},
+
+		get: {
+			needEntity: true,
+			permissions: ['domains.records.get']
+		},
+
+		update: {
+			needEntity: true,
+			permissions: ['domains.records.update']
+		},
+
+		replace: false,
+
+		remove: {
+			needEntity: true,
+			permissions: ['domains.records.remove']
+		},
+		syncRecords: {
+			params: {},
+			async handler(ctx) {
+				return this.findEntities(ctx, {});
+			}
+		},
+		resolveRecord: {
+			params: {
+				fqdn: { type: "string", optional: true },
+				type: { type: "string", optional: true },
+				data: { type: "string", optional: true },
+			},
+			async handler(ctx) {
+				const params = Object.assign({}, ctx.params);
+				return this.findEntity(null, {
+					query: {
+						...params
+					}
+				});
+			}
+		},
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+		async "domains.created"(ctx) {
+			const domain = ctx.params.data;
+			return this.populateDomainRecords(ctx, domain)
+		},
+		async "domains.removed"(ctx) {
+			const domain = ctx.params.data;
+			try {
+				const entities = await this.findEntities(ctx, {
+					query: { domain: domain.id },
+					fields: ["id"],
+					scope: false
+				});
+				await this.Promise.all(
+					entities.map(entity => this.removeEntity(ctx, { id: entity.id, scope: false }))
+				);
+			} catch (err) {
+				this.logger.error(`Unable to delete records of domain '${domain.id}'`, err);
+			}
+		},
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+		async populateDomainRecords(ctx, domain) {
+
+			const promises = []
+
+			if (this.config["domains.autoPopulate"]) {
+				const soa = {
+					fqdn: domain.domain,
+					type: "SOA",
+					ttl: 3600,
+					admin: this.config["domains.hostmaster"],
+					serial: 2003080800,
+					refresh: 86400,
+					retry: 900,
+					expiration: 1209600,
+					minimum: 86400
+				}
+
+				soa.data = this.config["domains.domain"];
+
+				promises.push(this.actions.create(soa, { parentCtx: ctx }))
+
+				if (this.config["domains.issuewild"]) {
+					promises.push(this.actions.create({
+						fqdn: domain.domain,
+						type: "CAA",
+						flag: 0,
+						tag: 'issuewild',
+						data: this.config["domains.issuewild"]
+					}, { parentCtx: ctx }))
+				}
+
+				const mainDomain = await ctx.call('v1.domains.getDomain', {
+					domain: this.config['domains.domain']
+				})
+				if (mainDomain) {
+					const nameservers = await this.findEntities(null, {
+						query: {
+							domain: mainDomain.id,
+							type: 'NS'
+						}
+					});
+
+					for (let index = 0; index < nameservers.length; index++) {
+						const nameserver = nameservers[index];
+						promises.push(this.actions.create({
+							fqdn: `${domain.domain}`,
+							type: "NS",
+							data: nameserver.data,
+						}, { parentCtx: ctx }))
+					}
+				}
+			}
+			return Promise.all(promises)
+		},
+		async validateHasDomainPermissions(query, ctx, params) {
+			// Adapter init
+			if (!ctx) return query;
+
+			if (params.domain) {
+				console.log(params)
+				const res = await ctx.call("v1.domains.resolve", {
+					id: params.domain,
+					fields: ['id']
+				});
+				if (res) {
+					query.domain = params.domain;
+					return query;
+				}
+				throw new MoleculerClientError(
+					`You have no right for the domain '${params.domain}'`,
+					403,
+					"ERR_NO_PERMISSION",
+					{ domain: params.domain }
+				);
+			}
+			if (!ctx.action) {
+				query.domain = params.domain;
+				return query;
+			}
+			if (ctx.action.params.domain && !ctx.action.params.domain.optional) {
+				throw new MoleculerClientError(`domain is required`, 422, "VALIDATION_ERROR", [
+					{ type: "required", field: "domain" }
+				]);
+			}
+		},
+
+		async validateDomain({ ctx, value, params, id, entity }) {
+			return ctx.call("v1.domains.getDomain", { fqdn: params.fqdn, member: ctx.meta.userID })
+				.then((res) => res ? true : `No permissions '${value} not found'`)
+		},
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() { },
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() { },
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() { }
+};
---
+++ services/domains.service.js
@@ -0,0 +1,506 @@
+"use strict";
+
+const DbService = require("db-mixin");
+const ConfigLoader = require("config-mixin");
+
+const Membership = require("membership-mixin");
+
+const psl = require('../lib/psl.min.js')
+
+/**
+ * Addons service
+ */
+module.exports = {
+    name: "domains",
+    version: 1,
+
+    mixins: [
+        DbService({
+            entityChangedEventMode: 'emit'
+        }),
+        ConfigLoader(['domains.**']),
+        Membership({
+            permissions: 'domains'
+        })
+    ],
+
+    /**
+     * Service dependencies
+     */
+    dependencies: [
+        { name: "accounts", version: 1 }
+    ],
+
+    /**
+     * Service settings
+     */
+    settings: {
+        rest: true,
+
+        fields: {
+            id: {
+                type: "string",
+                primaryKey: true,
+                secure: true,
+                columnName: "_id"
+            },
+            default: {
+                type: "boolean",
+                default: false
+            },
+            domain: {
+                type: "string",
+                required: true,
+                trim: true,
+                empty: false,
+                onCreate: ({ ctx, value }) => psl.parse(value.replace('*.', '').replace('_', '')).domain,
+                validate: "validateDomain",
+            },
+
+            name: {
+                type: "string"
+            },
+
+            tld: {
+                type: "string",
+                readonly: true,
+                onCreate: ({ ctx, params, value }) => psl.parse(params.domain.replace('*.', '').replace('_', '')).tld
+            },
+            sld: {
+                type: "string",
+                readonly: true,
+                onCreate: ({ ctx, params, value }) => psl.parse(params.domain.replace('*.', '').replace('_', '')).sld
+            },
+
+
+            description: {
+                type: "string",
+                required: false,
+                trim: true,
+            },
+
+            recordCount: {
+                type: "number",
+                virtual: true,
+                populate: function (ctx, values, entities, field) {
+                    return Promise.all(
+                        entities.map(async entity => {
+                            return await ctx.call("v1.domains.records.count", { domain: this.encodeID(entity._id) })
+                        })
+                    );
+                }
+            },
+
+            records: {
+                type: "array",
+                items: { type: "string", empty: false },
+                readonly: true,
+                populate: {
+                    action: "v1.domains.records.list",
+                    params: {
+                        fields: ["id", "name"]
+                    }
+                }
+            },
+
+
+            ...Membership.FIELDS,
+
+            options: { type: "object" },
+            createdAt: {
+                type: "number",
+                readonly: true,
+                onCreate: () => Date.now()
+            },
+            updatedAt: {
+                type: "number",
+                readonly: true,
+                onUpdate: () => Date.now()
+            },
+            deletedAt: {
+                type: "number",
+                readonly: true,
+                hidden: "byDefault",
+                onRemove: () => Date.now()
+            }
+        },
+        defaultPopulates: ["records"],
+
+        scopes: {
+            // List the not deleted addons
+            notDeleted: { deletedAt: null },
+
+            ...Membership.SCOPE,
+        },
+
+        defaultScopes: ["notDeleted", ...Membership.DSCOPE],
+
+        config: {
+            "domains.domain": "example.com",
+            "domains.hostmaster": "hostmaster@example.com",
+            "domains.issuewild": "letsencrypt.org",
+            "domains.autoPopulate": false
+        }
+    },
+
+    /**
+     * Actions
+     */
+    actions: {
+        create: {
+            permissions: ['domains.create']
+        },
+        list: {
+            permissions: ['domains.list']
+        },
+        find: {
+            rest: "GET /find",
+            permissions: ['domains.find']
+        },
+        count: {
+            rest: "GET /count",
+            permissions: ['domains.count']
+        },
+        get: {
+            needEntity: true,
+            permissions: ['domains.get']
+        },
+        resolve: {
+            needEntity: true,
+            permissions: ['domains.get']
+        },
+        update: {
+            needEntity: true,
+            permissions: ['domains.update']
+        },
+        replace: false,
+        remove: {
+            needEntity: true,
+            permissions: ['domains.remove']
+        },
+
+        domainExists: {
+            params: {
+                fqdn: "string"
+            },
+            async handler(ctx) {
+                const params = Object.assign({}, ctx.params);
+
+                const parsed = psl.parse(params.fqdn.replace('*.', '').replace('_', ''));
+
+                const domain = await this.findEntity(ctx, {
+                    query: {
+                        domain: parsed.domain
+                    }
+                });
+                return !!domain
+            }
+        },
+        getDomain: {
+            params: {
+                owner: { type: "string", optional: true },
+                member: { type: "string", optional: true },
+                fqdn: { type: "string", optional: true },
+                id: { type: "string", optional: true }
+            },
+            async handler(ctx) {
+                const params = Object.assign({}, ctx.params);
+
+                const query = {}
+
+
+                if (params.fqdn) {
+                    const parsed = psl.parse(params.fqdn.replace('*.', '').replace('_', ''));
+                    query.domain = parsed.domain;
+                }
+
+                if (params.id) {
+                    query.id = params.id
+                }
+
+                if (params.members) {
+                    query.members = params.members
+                } else if (params.owner) {
+                    query.owner = params.owner
+                }
+                console.log(query)
+                return this.findEntity(ctx, {
+                    query: query
+                });
+            }
+        },
+        resolveDomain: {
+            params: {
+                domain: { type: "string", optional: false },
+            },
+            async handler(ctx) {
+                const params = Object.assign({}, ctx.params);
+
+                const parsed = psl.parse(params.domain.replace('*.', '').replace('_', ''));
+
+
+                const query = { domain: parsed.domain }
+
+                return this.findEntity(ctx, {
+                    query,
+                    scope: false
+                });
+            }
+        },
+
+        records: {
+            rest: "GET /records",
+            params: {
+
+            },
+            permissions: ['domains.records.get'],
+            async handler(ctx) {
+                const params = Object.assign({}, ctx.params);
+                const domainName = this.config["domains.domain"];
+
+                const domain = await ctx.call('v1.domains.resolveDomain', {
+                    domain: domainName
+                })
+
+                return ctx.call('v1.domains.records.list', { pageSize: 100, domain: domain.id })
+            }
+        },
+        resolveRecord: {
+            description: "Archive the addon",
+            rest: "GET /records/:record",
+            params: {
+                record: { type: "string", optional: false },
+            },
+            permissions: ['domains.records.get'],
+            async handler(ctx) {
+                const params = Object.assign({}, ctx.params);
+                const domainName = this.config["domains.domain"];
+
+                const domain = await ctx.call('v1.domains.resolveDomain', {
+                    domain: domainName
+                })
+
+                return ctx.call('v1.domains.records.resolve', { id: params.record, domain: domain.id })
+            }
+        },
+        addRecord: {
+            params: {
+                fqdn: { type: "string", optional: false },
+                data: { type: "string", optional: false },
+                type: {
+                    type: "enum",
+                    values: ["A", "AAAA", "CNAME", "NS", "TXT"],
+                    optional: true,
+                    default: 'A'
+                },
+            },
+            permissions: ['domains.addARecord'],
+            async handler(ctx) {
+                const params = Object.assign({}, ctx.params);
+
+                const parsed = psl.parse(params.fqdn.replace('*.', '').replace('_', ''));
+
+                const domain = await this.findEntity(null, {
+                    query: {
+                        domain: parsed.domain,
+                        deletedAt: null
+                    },
+                    scope: false
+                });
+
+                const options = { meta: { userID: domain.owner } };
+
+                const data = {
+                    fqdn: params.fqdn,
+                    type: params.type,
+                    data: params.data
+                }
+
+                let record = await ctx.call(`v1.domains.records.find`, {
+                    query: {
+                        ...data,
+                    },
+                    domain: domain.id
+                }, options).then((res) => res.shift());
+                if (!record) {
+                    record = await ctx.call(`v1.domains.records.create`, {
+                        ...data,
+                        domain: domain.id
+                    }, options)
+                }
+
+                return record
+            }
+        },
+        removeRecord: {
+            params: {
+                fqdn: { type: "string", optional: false },
+                data: { type: "string", optional: false },
+                type: {
+                    type: "enum",
+                    values: ["A", "AAAA", "CNAME", "NS", "TXT"],
+                    optional: true,
+                    default: 'A'
+                },
+            },
+            permissions: ['domains.addARecord'],
+            async handler(ctx) {
+                const params = Object.assign({}, ctx.params);
+
+                const parsed = psl.parse(params.fqdn.replace('*.', '').replace('_', ''));
+
+                const domain = await this.findEntity(null, {
+                    query: {
+                        domain: parsed.domain,
+                        deletedAt: null
+                    },
+                    scope: false
+                });
+
+                const options = { meta: { userID: domain.owner } };
+
+                const data = {
+                    fqdn: params.fqdn,
+                    type: params.type,
+                    data: params.data
+                }
+                let record = await ctx.call(`v1.domains.records.find`, {
+                    query: {
+                        ...data
+                    },
+                    domain: domain.id
+                }, options).then((res) => res.shift());
+                if (record) {
+                    await ctx.call(`v1.domains.records.remove`, {
+                        id: record.id,
+                        domain: domain.id
+                    }, options)
+                }
+
+                return record
+            }
+        },
+
+
+        sync: {
+            rest: "GET /sync",
+            params: {
+                target: { type: "string", min: 3, optional: true },
+            },
+            permissions: ['domains.sync'],
+            async handler(ctx) {
+
+                return this.scrapeAgents(ctx, 'v1.ddns.agent.sync')
+            }
+        },
+        maps: {
+            rest: "GET /maps",
+            params: {
+                target: { type: "string", min: 3, optional: true },
+            },
+            permissions: ['domains.maps'],
+            async handler(ctx) {
+
+                return this.scrapeAgents(ctx, 'v1.ddns.agent.maps')
+            }
+        },
+        stats: {
+            rest: "GET /stats",
+            params: {
+                target: { type: "string", min: 3, optional: true },
+            },
+            permissions: ['domains.stats'],
+            async handler(ctx) {
+                return this.scrapeAgents(ctx, 'v1.ddns.agent.stats')
+            }
+        },
+    },
+
+    /**
+     * Events
+     */
+    events: {
+        
+    },
+
+    /**
+     * Methods
+     */
+    methods: {
+        async scrapeAgents(ctx, action, params = {}) {
+            const list = await ctx.call("$node.list");
+
+            const result = [];
+            const promises = [];
+            for (let index = 0; index < list.length; index++) {
+                const node = list[index];
+                promises.push(ctx.call(action, params, { nodeID: node.id }));
+            }
+
+            const settled = await Promise.allSettled(promises);
+            for (let index = 0; index < list.length; index++) {
+                const node = list[index];
+                result.push({
+                    nodeID: node.id,
+                    status: settled[index].status,
+                    info: settled[index].value,
+                    reason: settled[index].reason,
+                });
+            }
+
+            return result
+        },
+        validateDomain({ ctx, params, value }) {
+            return this.countEntities(ctx, {
+                query: {
+                    domain: value
+                },
+            }, { transform: false })
+                .then(res =>
+                    res == 0
+                        ? true
+                        : `Domain '${value}' already managed.`
+                )
+        },
+        /**
+         * Validate the `owner` property of addon.
+         */
+        validateOwner({ ctx, value }) {
+            return ctx
+                .call("v1.accounts.resolve", {
+                    id: value,
+                    throwIfNotExist: true,
+                    fields: ["status"]
+                })
+                .then(res =>
+                    res && res.status == 1
+                        ? true
+                        : `The owner '${value}' is not an active user.`
+                )
+            //.catch(err => err.message);
+        },
+        async seedDB() {
+            for (const [key, value] of Object.entries(this.settings.config || {})) {
+                const found = await this.broker.call('v1.config.get', { key });
+                if (found == null) {
+                    await this.broker.call('v1.config.set', { key, value });
+                }
+            }
+        }
+    },
+
+    /**
+     * Service created lifecycle event handler
+     */
+    created() { },
+
+    /**
+     * Service started lifecycle event handler
+     */
+    started() { },
+
+    /**
+     * Service stopped lifecycle event handler
+     */
+    stopped() { }
+};
